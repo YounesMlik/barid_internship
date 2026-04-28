90863470f065b119ea40b2ff0e96fc4927779e8a
YounesMlik <159716598+YounesMlik@users.noreply.github.com>
2026-04-28 11:48:40 +0100

    output xlsx instead of csv

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ our_locations = [
 DEFAULT_DELAY = 7  # days
 
 DEFAULT_INPUT_PATH = "smi_suiviexpedition.csv"
-DEFAULT_OUTPUT_PATH = "rapport_retard.csv"
+DEFAULT_OUTPUT_PATH = "rapport_retard.xlsx"
 
 
 def get_input_file():
@@ -71,7 +71,7 @@ def main():
     data = load_data(input_file)
     result = filter_data(data, delay_threshold)
 
-    result.write_csv(DEFAULT_OUTPUT_PATH)
+    result.write_excel(DEFAULT_OUTPUT_PATH)
 
 
 if __name__ == "__main__":
