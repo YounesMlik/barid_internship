643c3fd694d17eefb4b6fa6df08ba334879d8a7b
Younes Mlik <younesmlik88@gmail.com>
2026-05-13 20:53:09 +0100

    refactor offline_historique

--- offline_historique.py
+++ offline_historique.py
@@ -1,8 +1,8 @@
 import polars as pl
 from pathlib import Path
 from bs4 import BeautifulSoup, Tag
-from typing import Callable, Iterable
-from dataclasses import dataclass
+from typing import Callable, Iterable, Any
+from dataclasses import dataclass, field as dc_field
 from types import SimpleNamespace
 
 
@@ -10,6 +10,7 @@ from types import SimpleNamespace
 class TableConfig:
     table_id: str
     schema: pl.Schema
+    renames: dict[str, str] = dc_field(default_factory=dict)
     transforms: Iterable[pl.Expr] = ()
 
 
@@ -37,10 +38,17 @@ OPERATIONS_TABLE = TableConfig(
             ("ORIGINE", pl.String),
         ]
     ),
+    renames={
+        "Date operation": "Date_operation",
+        "Heure Syst.Oper.": "Heure_Syst_Oper",
+        "Agent oper.": "Agent_oper",
+        "Date Etat": "Date_Etat",
+        "Agent maj": "Agent_maj",
+    },
     transforms=[
-        pl.col("Date operation").str.to_date("%d-%m-%Y"),
-        pl.col("Heure Syst.Oper.").str.to_datetime("%d-%m-%Y %H:%M"),
-        pl.col("Agent oper.").cast(pl.Int64),
+        pl.col("Date_operation").str.to_date("%d-%m-%Y"),
+        pl.col("Heure_Syst_Oper").str.to_datetime("%d-%m-%Y %H:%M"),
+        pl.col("Agent_oper").cast(pl.Int64),
         pl.col("Etat").str.extract(r"\[\s*(.*?)\s*\]", 1).alias("status_code"),
         pl.col("Etat").str.extract(r"\((.*?)\)", 1).alias("is_valid"),
     ],
@@ -59,9 +67,15 @@ SERVICES_TABLE = TableConfig(
             ("ORIGINE", pl.String),
         ]
     ),
+    renames={
+        "Libellé service": "Libelle_service",
+        "Num compte": "Num_compte",
+        "Mnt à percevoir": "Mnt_a_percevoir",
+        "Taxe TTC": "Taxe_TTC",
+    },
     transforms=[
-        pl.col("Mnt à percevoir").cast(pl.Float64),
-        pl.col("Taxe TTC").cast(pl.Float64),
+        pl.col("Mnt_a_percevoir").cast(pl.Float64),
+        pl.col("Taxe_TTC").cast(pl.Float64),
     ],
 )
 
@@ -82,9 +96,15 @@ DELIVERY_TABLE = TableConfig(
             ("N°chéque/N°reçu.TPE", pl.String),
         ]
     ),
+    renames={
+        "date.transf.ccp": "date_transf_ccp",
+        "n°pid": "n_pid",
+        "N°compte/Infos": "N_compte_Infos",
+        "N°chéque/N°reçu.TPE": "N_cheque_N_reçu_TPE",
+    },
     transforms=[
         pl.col("date_liv").str.to_date("%d/%m/%Y"),
-        pl.col("date.transf.ccp").str.to_date("%d/%m/%Y"),
+        pl.col("date_transf_ccp").str.to_date("%d/%m/%Y"),
         pl.col("montant").cast(pl.Float64),
     ],
 )
@@ -97,12 +117,12 @@ TABLES = SimpleNamespace(
 
 FIELDS = [
     FieldConfig("CodeBordereau", "Cab"),
+    FieldConfig("IdBordereau", "Id"),
     FieldConfig("date_op", "Date depot"),
     FieldConfig("Type_bordereau", "Type cab"),
     FieldConfig("Dernier_staut", "Dernier statut"),
     FieldConfig("regime", "Régime"),
     FieldConfig("contrat", "Contrat"),
-    FieldConfig("IdBordereau", "Id"),
     FieldConfig("heure_op", "Etat"),
     FieldConfig("Poids_global", "Poids global(en KG)"),
     FieldConfig("entite_dep", "Centre/Agence depot"),
@@ -125,35 +145,42 @@ EXTRACTORS = {
 }
 
 
-def extract_table(
-    soup: BeautifulSoup,
-    config: TableConfig,
-    additional_cols={},
-) -> pl.DataFrame:
-
-    table = soup.find("table", id=config.table_id)
-
-    if table is None:
-        return pl.DataFrame(schema=config.schema)
-
-    rows = [
+def parse_table_rows(table: Tag):
+    return [
         [cell.get_text(strip=True) for cell in row.find_all(["th", "td"])]
         for row in table.find_all("tr")
     ]
 
-    if not rows:
-        return pl.DataFrame(schema=config.schema)
 
-    headers, *data = rows
+def extract_table(
+    soup: BeautifulSoup,
+    config: TableConfig,
+    additional_cols: dict[str, Any] = {},
+) -> pl.DataFrame:
+    additional_cols_list = [pl.lit(v).alias(k) for k, v in additional_cols.items()]
 
-    return (
-        pl.DataFrame(data, schema=headers, orient="row")
-        .cast(config.schema)
+    table = soup.find("table", id=config.table_id)
+
+    if table is None:
+        rows = []
+    else:
+        rows = parse_table_rows(table)
+
+    data = rows[1:]
+
+    df = (
+        pl.DataFrame(data, schema=config.schema, orient="row")
+        .rename(config.renames)
         .with_columns(
             *config.transforms,
-            **{k: pl.lit(v) for k, v in additional_cols.items()},
+        )
+        .select(
+            *additional_cols_list,
+            pl.all(),
         )
     )
+
+    return df
 
 
 def extract_fields(
@@ -161,10 +188,18 @@ def extract_fields(
     fields: list[FieldConfig] = FIELDS,
     extractors: dict[str, Extractor] = EXTRACTORS,
 ):
+    elements_by_id = {
+        elem.get("id"): elem
+        for elem in soup.find_all(id=True)
+    }
+    
     data: dict[str, str] = {}
 
     for field in fields:
-        elem = soup.select_one(f"#{field.id}")
+        # elem = soup.select_one(f"#{field.id}")
+        # elem = soup.find(id=field.id)
+        elem = elements_by_id.get(field.id)
+        
         method = field.method
         extractor = extractors[method]
 
@@ -194,13 +229,33 @@ def extract_fields(
     return df
 
 
-def extract_page(file_path: str):
-    file = Path(file_path)
-    html = file.read_text()
-    soup = BeautifulSoup(html)
-    tables = [
-        extract_table(soup, config)
-        for config in [TABLES.operations, TABLES.services, TABLES.delivery]
-    ]
+def extract_page(html: str):
+    soup = BeautifulSoup(html, "lxml")
     fields = extract_fields(soup)
-    return tables, fields
+    cab = fields["Cab"][0]
+    id = fields["Id"][0]
+    tables = {
+        k: extract_table(soup, config, additional_cols={"cab": cab, "id": id})
+        for k, config in vars(TABLES).items()
+    }
+    return {"fields": fields, **tables}
+
+
+def extract_pages(htmls: Iterable[str]):
+    tables_list: dict[str, list[pl.DataFrame]] = {
+        "fields": [],
+        "operations": [],
+        "services": [],
+        "delivery": [],
+    }
+    for html in htmls:
+        page_data = extract_page(html)
+        for key in tables_list:
+            tables_list[key].append(page_data[key])
+
+    return {key: pl.concat(dfs) if dfs else None for key, dfs in tables_list.items()}
+
+
+def extract_pages_from_folder(folder_path: str | Path):
+    files = Path(folder_path).iterdir()
+    return extract_pages((file.read_text() for file in files))
